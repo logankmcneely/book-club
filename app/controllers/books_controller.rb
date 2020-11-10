15f63c53771ed2f518dd7ad412a49ce90274c3f8
Logan McNeely <logan.k.mcneely@gmail.com>
2020-11-10 10:58:44 -0600

    initial book show page commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,17 @@
 class BooksController < ApplicationController
 
+  def show
+    begin
+      @book = Book.lookup_book(params[:id])
+    rescue
+      flash[:alert] = "Invalid book"
+      redirect_to me_path
+    end
+  end
+
   def search
     if params[:book].present?
-      @results = Book.search(params[:book])
+      @results = Book.search_books(params[:book])
       respond_to do |format|
         format.js { render partial: 'books/search_results' }
       end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,14 @@ class Book < ApplicationRecord
   has_many :user_books
   has_many :users, through: :user_books
 
-  def self.search(query)
+  def self.search_books(query)
     client = Goodreads.new(api_key: Rails.application.credentials.goodreads[:key])
     client.search_books(query).results.work
+  end
+
+  def self.lookup_book(query)
+    client = Goodreads.new(api_key: Rails.application.credentials.goodreads[:key])
+    client.book(query)
   end
 
 end
